2c7dfed5a173a975e7b64f370e853ee00e3c2d95
epam <epam@local.rsu.edu.ru>
2018-10-05 20:19:56 +0300

    abc

---
+++ src/ex2_js-basics-part2/task02.js
@@ -0,0 +1,7 @@
+var f = function(Array)
+{
+    for(var i = 0;i<Array.length;i++) console.log(Array[i]);
+    console.log(Array.length);
+}
+
+f([1,2,3,4,5,"a",null,"c",[0,1]]);
---
+++ src/ex2_js-basics-part2/task03.js
@@ -0,0 +1,13 @@
+var f = function(Array)
+{
+    var even=0,noteven=0,zero=0;
+    for(var i = 0;i< Array.length;i++)
+    {
+        if(Array[i] === 0) zero++;
+        else 
+            if(Array[i] %2 === 0)even++;
+            else noteven++;
+    }
+    return [pos,neg,zero];
+}
+console.log(f([1,2,3,-2,-3,0,0,0,0,0,-8,null,true,false,[1],"",""]));
