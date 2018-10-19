47aed4396557f0eb3112c01e03ccccaeb463c765
Stanislav Akinin <hannonar@yandex.ru>
2018-10-19 03:09:19 +0300

    tsk14

--- src/ex4_js-objects-part2/task13.js
+++ src/ex4_js-objects-part2/task13.js
@@ -1,6 +1,6 @@
 var f = function rnd(min,max)
 {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() *99);
 }
 
-console.log(f(-5000,1000));
+console.log(f());
