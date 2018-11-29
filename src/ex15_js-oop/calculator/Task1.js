e3e165256cfbd25f64da82ffe565323c48ea6e91
Stanislav Akinin <hannonar@yandex.ru>
2018-11-29 19:10:33 +0300

    upd

--- src/ex15_js-oop/calculator/Task1.js
+++ src/ex15_js-oop/calculator/Task1.js
@@ -75,13 +75,19 @@ GraphicalCalculator.prototype = Object.create(ProgrammableCalculator.prototype);
 GraphicalCalculator.prototype.drawGraphic = function(){}
 
 var calc = new Calculator();
+var calc2 = new Calculator();
+
 var res = calc.add(100)
 .multiply(2)
 .divide(20)
 .reset()
 .subtract(5)
 .getResult();
+var res2 = calc2.add(123)
+.subtract(5)
+.getResult();
 
-console.log(result); // -1
+console.log(res);
+console.log(res2);
 
 console.log(calc.setState(321).getResult());
