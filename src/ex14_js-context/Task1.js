3edc315410cded1aa0b958d5c1f48eebde56a4e1
Stanislav Akinin <hannonar@yandex.ru>
2018-12-06 00:41:33 +0300

    in work

---
+++ src/ex14_js-context/Task1.js
@@ -0,0 +1,54 @@
+Calculator = function()
+{
+    result = 0;
+};
+
+Calculator.prototype.add = function(b)
+{
+    if(b!= undefined) result +=b;
+    return this;
+};
+Calculator.prototype.subtract = function(b)
+{
+    if(b!= undefined) result -=b;
+     return this;
+};
+Calculator.prototype.divide = function(b)
+{
+    if(b!= undefined) result /=b;
+    return this;
+};
+Calculator.prototype.multiply = function(b)
+{
+    if(b!= undefined) result *=b;
+    return this;
+};
+Calculator.prototype.getResult = function()
+{
+    return result;
+};
+Calculator.prototype.reset = function()
+{
+    result = 0;
+    return this;
+};
+Calculator.prototype.setState = function(b)
+{
+    result = b;
+    return this;
+};
+Calculator.prototype.fetchData = function(callback)
+{
+    console.log("Fetching data");
+    setTimeout(function(){console.log("Fetching completed");},1500);
+    callback();
+}
+Calculator.prototype.fetchingCompleted = function()
+{
+    this.setState(500);
+}
+
+var calc = new Calculator;
+//console.log(calc.add(2).add(3).divide(5).multiply(100).getResult());
+calc.fetchData(calc.fetchingCompleted);
+console.log(calc.getResult());
